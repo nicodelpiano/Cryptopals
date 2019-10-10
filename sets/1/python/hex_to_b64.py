784e80360782c5f75081ad9c70860d2607212f77
Nicolas Del Piano <ndel314@gmail.com>
2019-10-10 00:53:23 -0300

    Fix naming

--- sets/1/python/hex_to_b64.py
+++ sets/1/python/hex_to_b64.py
@@ -2,5 +2,5 @@ from codecs import encode, decode
 
 def hex_to_b64(hex_str):
   hex_bytes = decode(hex_str, 'hex')
-	b64_bytes = encode(hex_bytes, 'base64')
-	return b64_bytes.decode().replace("\n","")
+  b64_bytes = encode(hex_bytes, 'base64')
+  return b64_bytes.decode().replace("\n","")
